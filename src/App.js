41229e631ce62756c8718bd58559e673edc03158
leumanuel12 <cantiveros12@gmail.com>
2023-01-05 03:19:18 +0800

    transferred dictionary to pages folder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Employees from "./pages/Employees";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Customers from "./pages/Customers";
-import Dictionary from "./components/Dictionary";
+import Dictionary from "./pages/Dictionary";
 
 function App() {
   return (
